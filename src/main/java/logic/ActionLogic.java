1013978d1c2d7279ec173aa3856eb394b0c84bc3
Partizanux <partizanux@hotmail.com>
2015-08-27 00:25:38 +0300

    v

--- src/main/java/logic/ActionLogic.java
+++ src/main/java/logic/ActionLogic.java
@@ -75,7 +75,7 @@ public class ActionLogic {
 			ps.setString(1, date);
 			ps.setString(2, time);
 			
-			System.out.println(task);// ! ! !
+			System.out.println("In action logic: " + task);// ! ! !
 			
 			ps.setString(3, task);
 			ps.setInt(4, status);
